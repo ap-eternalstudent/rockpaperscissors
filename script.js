220a913995cbb1f596edbc3a97bfdff629fdcf46
Anton P <a.eternalstudent@gmail.com>
2021-10-03 11:08:31 +0300

    added a basic working gui

--- script.js
+++ script.js
@@ -1,28 +1,79 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const restartGame = document.querySelector(".restart-game");
+
+const playerScoreDisp = document.getElementById("player-score");
+const computerScoreDisp = document.getElementById("computer-score");
+
+const playerWonGamesDisp = document.getElementById("player-games-won");
+const computerWonGamesDisp = document.getElementById("computer-games-won");
+
+let playerChoice = "";
+let playerScore = 0;
+let computerScore = 0;
+let playerWonGames = 0;
+let computerWonGames = 0;
+
+restartGame.addEventListener("click", newGame);
+
+rock.addEventListener("click", function () {
+  playerChoice = "Rock";
+  game();
+});
+
+paper.addEventListener("click", function () {
+  playerChoice = "Paper";
+  game();
+});
+scissors.addEventListener("click", function () {
+  playerChoice = "Scissors";
+  game();
+});
+
 function computerPlay() {
   const move = ["Rock", "Paper", "Scissors"];
   return move[Math.floor(Math.random() * move.length)];
 }
 
-function playerSelection() {
-  let string = prompt(`Enter "Rock", "Paper" or "Scissors" `).toLowerCase();
-  let normalizedString = string.slice(0, 1).toUpperCase() + string.slice(1);
-  return normalizedString;
+function selectRock() {
+  playerChoice = "Rock";
+}
+function selectPaper() {
+  playerChoice = "Paper";
+}
+function selectScissors() {
+  playerChoice = "Scissors";
+}
+
+function newGame() {
+  resetGame();
+  playerWonGames = 0;
+  playerWonGamesDisp.textContent = 0;
+  computerWonGames = 0;
+  computerWonGamesDisp.textContent = 0;
+}
+
+function resetGame() {
+  playerScore = 0;
+  playerScoreDisp.textContent = playerScore;
+  computerScore = 0;
+  computerScoreDisp.textContent = computerScore;
 }
 
 function game() {
-  function playRound(playerSelection, computerPlay) {
+  function playRound(computerPlay) {
     let outcome = "";
-    console.log(outcome);
     if (
-      (playerSelection === "Paper" && computerPlay === "Rock") ||
-      (playerSelection === "Rock" && computerPlay === "Scissors") ||
-      (playerSelection === "Scissors" && computerPlay === "Paper")
+      (playerChoice === "Paper" && computerPlay === "Rock") ||
+      (playerChoice === "Rock" && computerPlay === "Scissors") ||
+      (playerChoice === "Scissors" && computerPlay === "Paper")
     ) {
       outcome = "Win";
     } else if (
-      (playerSelection === "Rock" && computerPlay === "Paper") ||
-      (playerSelection === "Scissors" && computerPlay === "Rock") ||
-      (playerSelection === "Paper" && computerPlay === "Scissors")
+      (playerChoice === "Rock" && computerPlay === "Paper") ||
+      (playerChoice === "Scissors" && computerPlay === "Rock") ||
+      (playerChoice === "Paper" && computerPlay === "Scissors")
     ) {
       outcome = "Lost";
     } else {
@@ -31,22 +82,36 @@ function game() {
 
     function whoWon() {
       if (outcome === "Lost") {
-        return alert(`You Lose! ${computerPlay} beats ${playerSelection}
-        `);
+        computerScore += 1;
+        computerScoreDisp.textContent = computerScore;
+        if (computerScore == 3) {
+          computerWonGames += 1;
+          computerWonGamesDisp.textContent = computerWonGames;
+          resetGame();
+          alert("Computer won the game");
+        } else {
+          return alert(`You Lose! ${computerPlay} beats ${playerChoice} this round
+            `);
+        }
       } else if (outcome === "Win") {
-        return alert(`You Won! ${playerSelection} beats ${computerPlay}`);
+        playerScore += 1;
+        playerScoreDisp.textContent = playerScore;
+        if (playerScore == 3) {
+          playerScore += 1;
+          playerWonGames += 1;
+          playerWonGamesDisp.textContent = playerWonGames;
+          resetGame();
+          alert("Player won the game");
+        } else {
+          return alert(
+            `You Won! ${playerChoice} beats ${computerPlay} this round`
+          );
+        }
       } else {
         return alert("Draw!!!! Rematch");
       }
     }
-
     whoWon();
   }
-  playRound(playerSelection(), computerPlay());
-}
-
-function runFiveTimes() {
-  for (let i = 0; i < 5; i++) {
-    game();
-  }
+  playRound(computerPlay());
 }
